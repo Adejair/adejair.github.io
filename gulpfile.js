b2a8a6e223e7fad8a632bd9e2b0dc97bd3ef7668
Adejair <adejair.junior@outlook.com>
2016-02-15 22:22:16 -0200

    Buttons, Avatar and Owner user menu concluido

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,94 @@
-var gulp        = require('gulp'),
-	plumber     = require('gulp-plumber'),
-	browserSync = require('browser-sync'),
-	stylus      = require('gulp-stylus'),
-	uglify      = require('gulp-uglify'),
-	concat      = require('gulp-concat'),
-	jeet        = require('jeet'),
-	rupture     = require('rupture'),
-	koutoSwiss  = require('kouto-swiss'),
-	prefixer    = require('autoprefixer-stylus'),
-	imagemin    = require('gulp-imagemin'),
-	cp          = require('child_process');
-
-var messages = {
-	jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+/*
+* @ Gulp file, Task Mannager.
+*/
 
 /**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', function (done) {
-	browserSync.notify(messages.jekyllBuild);
-	return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-		.on('close', done);
+* @ Inject direct files in build if this variable is true.
+*/
+const INJECT_BUILD = false;
+
+const gulp = require('gulp')
+  , stylus = require('gulp-stylus')
+  , browserSync = require('browser-sync')
+  , uglify = require('gulp-uglify')
+  , imagemin = require('gulp-imagemin')
+  , exec = require('child_process').exec;
+
+
+/**
+* @ Array with path of files  to watch
+*/
+const PATH_WATCH_HTML = ['./*.html', './_includes/*.html', './_layouts/*.html']
+  , PATH_WATCH_STYLUS = ['./src/stylus/**/*.styl', './src/stylus/*.styl']
+  , PATH_WATCH_JS = ['./src/js/**/*.js', './src/js/*.js']
+  , PATH_WATCH_PNG = ['./src/images/**/*.png', './src/images/*.png'];
+/**
+* @ Compile stylus file to css.
+*/
+gulp.task('stylus', () => {
+  return gulp.src('./src/stylus/*.styl')
+  .pipe(stylus())
+  .pipe(gulp.dest('./css'))
 });
 
 /**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-	browserSync.reload();
+* @ Build files to _site
+*/
+gulp.task('jekyll-build', (cb) => {
+  exec('jekyll build', (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr)
+    cb(err);
+  });
 });
 
 /**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['jekyll-build'], function() {
-	browserSync({
-		server: {
-			baseDir: '_site'
-		}
-	});
+* @ Create local server with resources of sync.
+*/
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: './_site'
+    }
+  });
 });
 
 /**
- * Stylus task
- */
-gulp.task('stylus', function(){
-		gulp.src('src/styl/main.styl')
-		.pipe(plumber())
-		.pipe(stylus({
-			use:[koutoSwiss(), prefixer(), jeet(),rupture()],
-			compress: true
-		}))
-		.pipe(gulp.dest('_site/assets/css/'))
-		.pipe(browserSync.reload({stream:true}))
-		.pipe(gulp.dest('assets/css'))
+* @ Minify files javascript
+*/
+gulp.task('minjs', () => {
+  return gulp.src('./src/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./assets/js'))
 });
 
 /**
- * Javascript Task
- */
-gulp.task('js', function(){
-	return gulp.src('src/js/**/*.js')
-		.pipe(plumber())
-		.pipe(concat('main.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('assets/js/'))
+* @ Minify images .png
+*/
+gulp.task('minimage', () => {
+  return gulp.src('./src/images/*.png')
+  // TODO: by default imagemin render value is : 3
+  .pipe(imagemin())
+  .pipe(gulp.dest('./assets/images'))
 });
 
 /**
- * Imagemin Task
- */
-gulp.task('imagemin', function() {
-	return gulp.src('src/img/**/*.{jpg,png,gif}')
-		.pipe(plumber())
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('assets/img/'));
-});
+* @ Build Jekyll files and reload browser.
+*/
+gulp.task('jekyll-reload', ['jekyll-build'], () => browserSync.reload());
 
 /**
- * Watch stylus files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
-gulp.task('watch', function () {
-	gulp.watch('src/styl/**/*.styl', ['stylus']);
-	gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-	gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-});
+* @ Watch Files and "compile" or reload.
+* FIXME: All files is sync twice because delay, first time page is reloaded and
+* reload is fired, now, .on('change') file is reloaded again to see changes.
+*/
 
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
+gulp.watch(PATH_WATCH_HTML, ['jekyll-reload']).on('change', browserSync.reload)
+gulp.watch(PATH_WATCH_STYLUS, ['stylus', 'jekyll-reload']).on('change', browserSync.reload);
+gulp.watch(PATH_WATCH_JS, ['minjs', 'jekyll-reload']).on('change', browserSync.reload);
+gulp.watch(PATH_WATCH_PNG, ['minimage', 'jekyll-reload']).on('change', browserSync.reload);
+
+
+
+
+gulp.task('default', ['stylus', 'minjs', 'minimage', 'jekyll-build', 'browser-sync']);
